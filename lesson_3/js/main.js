c526d155cd3760b4c400ee7664fe2e6a604becf4
Todor Nikolov <t.nikolov@tech-family.com>
2020-01-30 18:39:32 +0200

    Lesson 3 incomplete files

---
+++ lesson_3/js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+	//$(SELECTOR).doSomething();
+
+	// setTimeout -> delay execution
+	// setInterval -> execute over time
+
+	// p -> get me all paragraphs
+	// #musaka -> get me all elements with an id = "musaka"
+	// .musaka -> get me all elements with a class = "musaka"
+
+	/* Section 1 */
+
+    /* END Section 1 */
+
+	/* Section 2 */
+
+	/* END Section 2 */
+
+	/* Section 3 */
+
+	/* END Section 3 */
+
+	/* Section 4 */
+
+	/* END Section 4 */
+
+	/* Section 5 */
+
+	/* END Section 5 */
+
+	/* Section 6 */
+
+	/* END Section 6 */
+
+	/* Section 7 */
+
+	/* END Section 7 */
+
+	/* Section 8 */
+
+	/* END Section 8 */
+});
