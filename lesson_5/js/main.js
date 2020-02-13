0fc58529594cd5e97483f6ae81dbd911072b39bc
Todor Nikolov <t.nikolov@tech-family.com>
2020-02-13 20:51:30 +0200

    chess with pawns implement

--- lesson_5/js/main.js
+++ lesson_5/js/main.js
@@ -1,9 +1,48 @@
 const $document = $(document);
 let player_turn = 'w'; //w - whites; b - blacks
 
-function piece_click() {}
+function piece_click() {
+	const piece = $(this);
+	const field = piece.parent();
 
-function change_turn() {}
+	const piece_data = piece.attr('id').split('_');
+	const field_data = field.attr('id').split('_');
+
+	const data = {
+		x: Number(field_data[0]),
+		y: Number(field_data[1]),
+		color: piece_data[0],
+		type: piece_data[1],
+	};
+
+	if (data.color === player_turn) {
+		const fields = $('.field');
+		fields.removeClass('greenBG');
+		fields.off('click');
+
+		if (field.hasClass('selectedPiece')) {
+			field.removeClass('selectedPiece');
+		}
+		else {
+			fields.removeClass('selectedPiece');
+			field.addClass('selectedPiece');
+			select_piece(data, field, piece);
+		}
+	}
+}
+
+function change_turn() {
+	const fields = $('.field');
+	const pieces = fields.find('.piece');
+
+	fields.off('click');
+	fields.removeClass('selectedPiece greenBG');
+
+	pieces.off('click');
+	pieces.click(piece_click);
+
+	player_turn = player_turn === 'w' ? 'b' : 'w';
+}
 
 function main() {
 	$('#main-content').empty();
--- lesson_5/js/move.js
+++ lesson_5/js/move.js
@@ -1,1 +1,11 @@
-function move() {}
+function move(field_new_id, data) {
+    const field_new_arr = field_new_id.split('_');
+
+    // $(`.piece#${data.color}_${data.type}`).remove();
+    $(`.field#${data.x}_${data.y}`).empty();
+    $(`.field#${field_new_arr[0]}_${field_new_arr[1]}`).empty();
+
+    create_figure(field_new_arr[0], field_new_arr[1], data.color + '_' + data.type);
+
+    change_turn();
+}
